1fa62898a8e89e68e2d443ec8c3ef7f1a4c45bd0
luo0412 <1095847440@qq.com>
2022-09-12 09:54:20 +0000

    adjust

---
+++ docs/assets/_...all_.6c6b4fe2.js
@@ -0,0 +1,12 @@
+
+/**
+ * 由 Fantastic-admin 提供技术支持
+ * Powered by Fantastic-admin
+ * https://fantastic-admin.netlify.app
+ * 代码仓库
+ * Gitee  https://gitee.com/hooray/fantastic-admin
+ * Github https://github.com/hooray/fantastic-admin
+ */
+    
+import{_ as a,A as e,bf as n,o as s,e as t,g as o,f as c,l,bg as r,M as i,N as d,q as u,t as f,Y as v,J as p}from"./index.0ad98fba.js";import{E as m}from"./el-button.1bc6eee2.js";import{_ as b}from"./index.eb126601.js";import"./index.8bfffa1c.js";import"./index2.a53d5bbc.js";import"./index2.4f1db918.js";const _=a=>(i("data-v-439c912c"),a=a(),d(),a),j={class:"notfound"},x={class:"content"},w=_((()=>c("h1",null,"404",-1))),I=_((()=>c("div",{class:"desc"},"抱歉，你访问的页面不存在",-1))),y={__name:"[...all]",setup(a){const r=u(),i=e({inter:null,countdown:5});function d(){r.push("/")}return n((()=>{clearInterval(i.value.inter)})),s((()=>{i.value.inter=setInterval((()=>{i.value.countdown--,0==i.value.countdown&&(clearInterval(i.value.inter),d())}),1e3)})),(a,e)=>{const n=b,s=m;return f(),t("div",j,[o(n,{name:"404",class:"icon"}),c("div",x,[w,I,o(s,{type:"primary",onClick:d},{default:l((()=>[v(p(i.value.countdown)+" 秒后，返回首页",1)])),_:1})])])}}};"function"==typeof r&&r(y);var g=a(y,[["__scopeId","data-v-439c912c"]]);export{g as default};
+//# sourceMappingURL=_...all_.6c6b4fe2.js.map
---
+++ docs/assets/index2.4f1db918.js
@@ -0,0 +1,12 @@
+
+/**
+ * 由 Fantastic-admin 提供技术支持
+ * Powered by Fantastic-admin
+ * https://fantastic-admin.netlify.app
+ * 代码仓库
+ * Gitee  https://gitee.com/hooray/fantastic-admin
+ * Github https://github.com/hooray/fantastic-admin
+ */
+    
+import{O as r,c as e,h as o}from"./index.0ad98fba.js";const t={prefix:Math.floor(1e4*Math.random()),current:0},a=Symbol("elIdInjection"),n=n=>{const s=r(a,t);return e((()=>o(n)||`el-id-${s.prefix}-${s.current++}`))};export{n as u};
+//# sourceMappingURL=index2.4f1db918.js.map
